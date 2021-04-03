db140cf88f620763b533c9dc3b125f6d0fe0edb2
aferrero2707 <aferrero1975@gmail.com>
2021-04-03 22:05:31 +0200

    [MCH] fixed build errors

--- Modules/MUON/MCH/include/MCH/LinkDef.h
+++ Modules/MUON/MCH/include/MCH/LinkDef.h
@@ -6,9 +6,8 @@
 //#pragma link C++ class o2::quality_control_modules::muonchambers::Decoder+;
 //#pragma link C++ class o2::quality_control_modules::muonchambers::MuonChambersMapping+;
 #pragma link C++ class o2::quality_control_modules::muonchambers::PedestalsTask + ;
-#pragma link C++ class o2::quality_control_modules::muonchambers::PhysicsTask + ;
 #pragma link C++ class o2::quality_control_modules::muonchambers::PhysicsTaskDigits + ;
-#pragma link C++ class o2::quality_control_modules::muonchambers::PhysicsTaskPreClusters + ;
+#pragma link C++ class o2::quality_control_modules::muonchambers::PhysicsTaskPreclusters + ;
 #pragma link C++ class o2::quality_control_modules::muonchambers::PedestalsCheck + ;
 #pragma link C++ class o2::quality_control_modules::muonchambers::PhysicsCheck + ;
 #pragma link C++ class o2::quality_control_modules::muonchambers::MCHPostProcessing + ;
